b3c8ac984882ba44301af8b94c40ca4a245ca39a
agill248 <aveerg367@deltalearns.ca>
2025-03-01 22:23:06 +0000

    Fixed the parts where I forgot (f" and got rid of the random thing popup and started making the choice selection menu as in the grocery store list practice.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import random # For the Recommended thing
 def remove_guest(invited_people: list, guest: str):
     if guest in invited_people:
         invited_people.remove(guest)
-        print("{guest} has been removed from your spectacualar Dinner!")
+        print(f"{guest} has been removed from your spectacualar Dinner!")
     else:
         print("Please select a guest who is in your list and that you want to kick out from the Dinner.")
     return invited_people
@@ -16,9 +16,9 @@ def display_guest(invited_people: list):
     if invited_people:
         print("\nHere is your current guest list!")
         for guest in invited_people:    
-            print("{guest}")
+            print(f"{guest}")
     else:
-        print("\nYour guest list is empty.")
+        print("\nYour guest list is empty. Get some people on there!")
 
 # Create a Function that tells the user a random recommended thing to do at the Dinner
 def recommend_thing():
@@ -31,13 +31,14 @@ def recommend_thing():
 # Replace Function that will Replace a person the user does not like
 def replace_guest(invited_people: list):
     display_guest(invited_people)
-    
     guest_remove = input("\nEnter the name of the guest you would like to replace? ").strip()
     if guest_remove not in invited_people:
         print(f"{guest_remove} is not in the guest list!")
         return
-
     new_guest = input("Enter the name of the new guest:")
+    invited_people.remove(guest_remove)
+    invited_people.append(new_guest)
+    print(f"{guest_remove} spot has been taken over by {new_guest}!")
 # Make a list of People Invited
 invited_people = []
 
@@ -49,8 +50,8 @@ print(f"Welcome {name}, lets start inviting people and make the TACOS!")
 # Ask the User the amount of guests to invite
 while True:
     try:
-        num_guests = input("How many guests would you like to invite?")
-        if num_guests <= 0:
+        num_guests = int(input("How many guests would you like to invite?"))
+        if num_guests <= 0: # FIX THIS AT HOME IT DIDNT WORK
             print("Please enter a positive number so that we can continue.")
         else:
             break      
@@ -62,5 +63,7 @@ for x in range(num_guests):
     guest_name = input("Enter the name of the guest you would like to invite to your party!").strip()
     invited_people.append(guest_name)
 
-# Display the list of invited people to the party
-display_guest(invited_people)
+# Make a choice selection menu for the user
+while True:
+    print("Okkk, here are the next choices for your party.. By the way I don't know anyone on your list!")
+    print("1")
